9fe7e9248e54a4e65293e7551ee4f7b3833f2ce3
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2023-01-11 13:39:54 +0500

    added extra funcionality

--- src/screens/Svoyak.tsx
+++ src/screens/Svoyak.tsx
@@ -1,19 +1,59 @@
 import { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import { styles } from "../styles/SSvoyak";
 
 const Svoyak = () => {
   const [title, setTitle] = useState("O'yin nomi");
   const [data, setData] = useState([
-    { id: 1, name: "1-ishtirokchi", scores: [] },
-    { id: 2, name: "2-ishtirokchi", scores: [40] },
-    { id: 3, name: "3-ishtirokchi", scores: [50] },
-    { id: 4, name: "4-ishtirokchi", scores: [] },
-    { id: 5, name: "5-ishtirokchi", scores: [] },
+    { id: 0, name: "1-ishtirokchi", scores: "", numberOfLines: 2 },
+    { id: 1, name: "2-ishtirokchi", scores: "", numberOfLines: 2 },
+    { id: 2, name: "3-ishtirokchi", scores: "", numberOfLines: 2 },
+    { id: 3, name: "4-ishtirokchi", scores: "", numberOfLines: 2 },
+    { id: 4, name: "5-ishtirokchi", scores: "", numberOfLines: 2 },
   ]);
 
+  const onChangeName = (name: string, id: number) => {
+    const newData = [...data];
+    newData.find((item) => item.id === id).name = name;
+    setData(newData);
+  };
+
+  const onChangeScore = (scores: string, id: number) => {
+    const pattern = /^[\-\+\s]*[\d\s]*[\-\+\s]*$/;
+    if (pattern.test(scores)) {
+      const newData = [...data];
+      const preScoresLength = newData.find((item) => item.id === id).scores
+        .length;
+      newData.find((item) => item.id === id).scores =
+        preScoresLength < scores.length && scores[scores.length - 1] === "0"
+          ? scores + " + "
+          : scores;
+      setData(newData);
+    }
+  };
+
+  const onGameFinished = () => {};
+
+  const onGamerAdded = () => {
+    if (data.length < 15) {
+      const newGamer = {
+        id: data.length,
+        name: `${data.length + 1}-ishtirokchi`,
+        scores: "",
+        numberOfLines: 2,
+      };
+      setData([...data, newGamer]);
+    }
+  };
+
   return (
-    <View>
+    <ScrollView>
       <TextInput
         style={styles.titleInput}
         value={title}
@@ -22,18 +62,31 @@ const Svoyak = () => {
       />
       {data &&
         data.length &&
-        data.map((part) => (
-          <View key={part.id} style={styles.scoresWrap}>
+        data.map((gamer) => (
+          <View key={gamer.id} style={styles.scoresWrap}>
             <TextInput
               style={styles.participantInput}
-              value={part.name + ":"}
-              onChangeText={setTitle}
+              value={gamer.name}
+              onChangeText={(name) => onChangeName(name, gamer.id)}
               maxLength={18}
             />
-            <Text style={styles.scores}>{part.scores.map((score) => score + " + ")}</Text>
+            <TextInput
+              style={styles.scores}
+              multiline={true}
+              value={gamer.scores}
+              onChangeText={(scores) => onChangeScore(scores, gamer.id)}
+            />
           </View>
         ))}
-    </View>
+      <View style={styles.extraButtons}>
+        <TouchableOpacity onPress={onGamerAdded} style={styles.addGamer}>
+          <Text style={styles.textInButton}>Qoʻshish</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onGameFinished} style={styles.endGame}>
+          <Text style={styles.textInButton}>Yakunlash</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
--- src/styles/SSvoyak.ts
+++ src/styles/SSvoyak.ts
@@ -13,11 +13,31 @@ export const styles = StyleSheet.create({
     marginTop: 15,
     paddingHorizontal: 10,
     display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: "column",
   },
   scores: {
+    fontSize: 20,
+  },
+  extraButtons: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 30,
+  },
+  addGamer: {
+    paddingVertical: 10,
+    borderRadius: 6,
+    paddingHorizontal: 20,
+    backgroundColor: "#0f0",
+  },
+  endGame: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 6,
+    backgroundColor: "#f00",
+  },
+  textInButton: {
+    color: "#fff",
     fontSize: 18,
-    marginLeft: 5,
   },
 });
